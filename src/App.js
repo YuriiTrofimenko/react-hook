8366c180ffc940bdca9fc213145dc177d02b0ed1
YuriiTrofimenko <tyaa@ukr.net>
2020-11-10 21:11:49 +0200

    init and demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [isVisible, setVisibility] = useState(true)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img style={{display: isVisible ? 'block' : 'none'}} src={logo} className="App-logo" alt="logo" />
+        <p onClick={()=>{setVisibility(!isVisible)}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
